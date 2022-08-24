d568441777679d9a1b775151af0297178d6cd689
Hema <hemasahu019@gmail.com>
2022-08-24 22:19:04 +0530

    Added Welcome Msg to main

---
+++ UserRegistrationWithMSTestNExceptiom/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationWithMSTestNExceptiom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem with Exception And Unit Tests");
+            Console.ReadLine();
+        }
+    }
+}
